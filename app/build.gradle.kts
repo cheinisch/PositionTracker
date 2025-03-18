3437e5d2b6ed277ceef90387bbc4a409a9fe1715
Christian Heinisch <chris@christian-heinisch.de>
2025-03-18 23:24:54 +0100

    Add App Logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.heimfisch.positiontracker"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.0.1.0"
+        versionCode = 4
+        versionName = "0.0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
